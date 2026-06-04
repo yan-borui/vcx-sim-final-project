bc5bb273e52a0abb65278101154a95ec03b98010
kangkang0430 <2442965055@qq.com>
2026-06-04 21:03:36 +0800

    Initialize Fluid Simulation app in main function

--- src/VCX/Labs/FinalProject/main.cpp
+++ src/VCX/Labs/FinalProject/main.cpp
@@ -1,3 +1,13 @@
+#include "Assets/bundled.h"
+#include "Labs/FinalProject/App.h"
+
 int main() {
-    return 0;
+    using namespace VCX;
+    return Engine::RunApp<Labs::FluidSimulation::App>(Engine::AppContextOptions {
+        .Title         = "VCX-sim Lab4: Final Project - Coupled Fluid-Rigid Simulation",
+        .WindowSize    = {1024, 768},
+        .FontSize      = 16,
+        .IconFileNames = Assets::DefaultIcons,
+        .FontFileNames = Assets::DefaultFonts,
+    });
 }
