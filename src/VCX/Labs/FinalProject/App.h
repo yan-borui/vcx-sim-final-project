17c46a8d7ed7c16ff94de30bce7e65abe6a3adfd
kangkang0430 <2442965055@qq.com>
2026-06-11 21:33:41 +0800

    Add new case classes to App.h for fluid simulation

--- src/VCX/Labs/FinalProject/App.h
+++ src/VCX/Labs/FinalProject/App.h
@@ -2,7 +2,11 @@
 
 #include <vector>
 #include "Engine/app.h"
+#include "Labs/FinalProject/FluidSimulator.h"
 #include "Labs/FinalProject/CaseCoupled.h"
+#include "Labs/FinalProject/CaseFreeSurfaceSeparation.h"
+#include "Labs/FinalProject/CaseSubgrid.h"
+#include "Labs/FinalProject/CaseVariation.h"
 #include "Labs/Common/UI.h"
 
 namespace VCX::Labs::FluidSimulation {
@@ -10,9 +14,15 @@ namespace VCX::Labs::FluidSimulation {
     private:
         Common::UI _ui;
         CaseFluid _caseFluid;
+        CaseSubgrid _caseSubgrid;
+        CaseFreeSurfaceSeparation _caseFreeSurfaceSeparation;
+        CaseVariation _caseVariation;
         std::size_t _caseId = 0;
         std::vector<std::reference_wrapper<Common::ICase>> _cases = {
+            _caseVariation,
             _caseFluid,
+            _caseSubgrid,
+            _caseFreeSurfaceSeparation,
         };
 
     public:
