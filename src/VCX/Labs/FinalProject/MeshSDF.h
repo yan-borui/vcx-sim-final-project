c3339d15c0d296e04908424cd3208f601a3d9c6e
吐泡泡 <2400013178@stu.pku.edu.cn>
2026-06-12 21:09:17 +0800

    Update MeshSDF.h

--- src/VCX/Labs/FinalProject/MeshSDF.h
+++ src/VCX/Labs/FinalProject/MeshSDF.h
@@ -18,9 +18,18 @@ namespace VCX::Labs::Final {
         bool  LoadOBJ(std::string const & filePath);
         float SignedDistance(glm::vec3 const & localPoint) const;
         bool  IsValid() const { return ! _triangles.empty(); }
+        glm::vec3 const & MinBounds() const { return _minBounds; }
+        glm::vec3 const & MaxBounds() const { return _maxBounds; }
 
     private:
         std::vector<MeshTriangle> _triangles;
+        glm::vec3                 _minBounds { 0.0f };
+        glm::vec3                 _maxBounds { 0.0f };
+        glm::vec3                 _gridMin { 0.0f };
+        glm::vec3                 _gridMax { 0.0f };
+        glm::vec3                 _gridDx { 1.0f };
+        int                       _gridResolution { 0 };
+        std::vector<float>        _distanceGrid;
 
         static float PointTriangleDistance(glm::vec3 const & p, MeshTriangle const & tri);
         static bool  RayIntersectsTriangle(
@@ -28,6 +37,10 @@ namespace VCX::Labs::Final {
              glm::vec3 const &    dir,
              MeshTriangle const & tri);
 
+        int   GridOffset(int x, int y, int z) const;
+        float ExactSignedDistance(glm::vec3 const & localPoint) const;
+        float OutsideBoundsDistance(glm::vec3 const & localPoint) const;
+        void  BuildDistanceGrid();
         bool IsInside(glm::vec3 const & p) const;
     };
 
